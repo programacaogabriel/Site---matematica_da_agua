464cf6ab5d62265d0f120f5ed7aa417dbe840e53
programacaogabriel <programacaogabriel@gmail.com>
2023-02-26 12:12:25 -0300

    :hammer: Resultado
    Feito melhorias no resultado final

--- script.js
+++ script.js
@@ -3,8 +3,18 @@ function calcular() {
 
     let resultado = (peso * 35)
     const resultadoFinal = resultado.toFixed()
+    const resultadoFinalLitros = resultadoFinal / 1000
 
-    document.getElementById("resultado").innerHTML = `O resultado é igual a ${resultadoFinal} litros`
+
+
+    if (resultadoFinal < 0) {
+        document.getElementById("resultado").innerHTML = `O número não pode ser menor que 0`
+    } else {
+
+        document.getElementById("resultado").innerHTML = `O resultado é igual a ${resultadoFinal} mL / ${resultadoFinalLitros} Litros`
+    }
+
+
 
 
 }
